d74aa120680666f97fc0ff28f9c8903d951c24ea
danh-nt <125142607+danh-nt@users.noreply.github.com>
2024-11-28 17:35:01 +0700

    Update StatisticsForm.Designer.cs

--- CD_Management/CD_Management/View/StatisticsForm.Designer.cs
+++ CD_Management/CD_Management/View/StatisticsForm.Designer.cs
@@ -117,9 +117,9 @@ namespace CD_Management.View
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(172, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 15;
-            this.label1.Text = "Vui lòng nhập ID";
+            this.label1.Text = "Vui lòng nhập mã";
             // 
             // textBoxSearch
             // 
