65318bf3db5a336e721c3be6989deeaff1ce1e5f
danh-nt <125142607+danh-nt@users.noreply.github.com>
2024-11-28 17:28:04 +0700

    Update StatisticsForm.cs

--- CD_Management/CD_Management/View/StatisticsForm.cs
+++ CD_Management/CD_Management/View/StatisticsForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
@@ -392,6 +392,6 @@ namespace CD_Management.View
             }
         }
 
-
+        
     }
 }
