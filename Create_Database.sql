9b695cab29aa978455e63804b79e53af02c201f2
danh-nt <125142607+danh-nt@users.noreply.github.com>
2024-12-01 12:23:33 +0700

    Update Create_Database.sql

--- Create_Database.sql
+++ Create_Database.sql
@@ -81,6 +81,9 @@ CREATE TABLE QuanLyKho (
     FOREIGN KEY (NhaCungCapId) REFERENCES NhaCungCap(MaNhaCungCap) ON DELETE SET NULL
 );
 
+INSERT INTO Account (Id, Name, Password, Role) VALUES
+('002', 'Nhom16', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 'admin');
+
 INSERT INTO Bang (MaBang, TenBang, SoLuong, DonGiaBan, TheLoai, TacGia) VALUES 
     ('BD001', N'Bên Trên Tầng Lầu', 50, 50000, N'Nhạc Trẻ', N'Tăng Duy Tân'),
     ('BD002', N'Hạ Cánh Nơi Anh', 20, 150000, N'Phim Tình Cảm', 'Park Ji-eun'),
